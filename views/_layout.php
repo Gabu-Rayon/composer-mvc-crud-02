4d5644313d0035b21cb0ebe14c37aa69d0671587
I am M Gibson M <73939430+Gabu-Rayon@users.noreply.github.com>
2023-08-15 15:22:31 +0300

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Composer MVC CRUD</title>
+
+    <!--Include Stylesheet---->
     <link href="/app.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
